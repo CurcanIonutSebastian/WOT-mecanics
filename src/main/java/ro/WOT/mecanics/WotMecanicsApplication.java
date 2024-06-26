e9f16e54a4f7dd087d160d82ef6b1324e7aa8e00
ionut <ionut02444@gmail.com>
2024-06-26 14:40:17 +0300

    first commit

---
+++ src/main/java/ro/WOT/mecanics/WotMecanicsApplication.java
@@ -0,0 +1,13 @@
+package ro.WOT.mecanics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WotMecanicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WotMecanicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/WOT/mecanics/WotMecanicsApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.WOT.mecanics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WotMecanicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
